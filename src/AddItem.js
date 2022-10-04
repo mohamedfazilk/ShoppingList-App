a9b6d4594ec3c19812b8c42dcbe6440e7e316481
mohamedfazilk <mohamedfazil961@gmail.com>
2022-10-04 16:49:33 -0700

    add dialogbox and two state values

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 function AddItem(props) {
+    const [open, setOpen] = useState(false);
+    const[item, setItem] = useState({
+        product:'',
+        amount:''
+    })
+    const handleOpen = () => {
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+    }
     return (
-        <div></div>
+
+        <div>
+            <Button onClick={handleOpen}>
+                Add Item
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>
+                    New Item
+                </DialogTitle>
+
+                <DialogContent>
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>
+                        Cancel
+                    </Button>
+
+                    <Button onClick={addItem}>
+                        Add
+                    </Button>
+
+                </DialogActions>
+            </Dialog>
+
+        </div>
     );
 }
 export default AddItem;
