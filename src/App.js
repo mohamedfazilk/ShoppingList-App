c169a2be34155bf7fb0086f73deac0a24a874380
mohamedfazilk <mohamedfazil961@gmail.com>
2022-10-04 16:01:13 -0700

    MUI AppBar component to show the toolbar in our app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
 import { Container } from "@mui/system";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 
 function App() {
   return (
     <div className="App">
-      <Container></Container>
+      <Container>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              Shopping List
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Container>
     </div>
   );
 }
